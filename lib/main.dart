d655bbcbaebbc04c351d361ad1cdfa678769d100
miraj489 <rahman15-4893@diu.edu.bd>
2025-02-18 12:53:06 +0600

    container

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 void main() {
-  runApp(const LabClass05());
+  runApp(const MyApp());
 }
 
-class LabClass05 extends StatelessWidget {
-  const LabClass05({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic Flutter UI-02",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Basic Flutter UI-02"),
-          backgroundColor: Colors.blue[400],
-          centerTitle: true,
+        backgroundColor: const Color(0xFFE3E8FF),
+        body: Center(
+          child: PriceCard(),
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal, 
-          child: Container(
-            height: 600,
-            //width: double.infinity,
+      ),
+    );
+  }
+}
+
+class PriceCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 500,
+      height: 600,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Align(
+            alignment: Alignment.topLeft,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-              crossAxisAlignment: CrossAxisAlignment.center, 
-              children: List.generate(
-                100,
-                (index) => Icon(
-                  Iconsax.add_circle,
-                  size: index.toDouble(), 
-                  color: const Color.fromARGB(255, 21, 0, 255),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "30% OFF",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
+                Icon(
+                  Icons.favorite_border,
+                  color: Colors.grey,
+                  size: 24,
+                ),
+              ],
             ),
           ),
-        ),
+          
+          Container(
+            width: 150,
+            height: 150,
+            child: CustomPaint(
+            ),
+          ),
+          
+          const Text(
+            " Air                                Shoes",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "1000",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(width: 15),
+              Text(
+                "1500",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey[400],
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
