6e83ed45053b6e09ab1565b8fcd2340c3326a866
miraj489 <rahman15-4893@diu.edu.bd>
2025-02-18 11:26:36 +0600

    side scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 void main() {
   runApp(const LabClass05());
@@ -13,37 +14,27 @@ class LabClass05 extends StatelessWidget {
       title: "Basic Flutter UI-02",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("5 Star"),
-          backgroundColor: const Color.fromARGB(255, 212, 66, 245),
+          title: Text("Basic Flutter UI-02"),
+          backgroundColor: Colors.blue[400],
           centerTitle: true,
         ),
-        body: Container(
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-               Icons.star,
-               size: 50,
-               color: const Color.fromARGB(255, 234, 234, 14),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal, 
+          child: Container(
+            height: 600,
+            //width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+              crossAxisAlignment: CrossAxisAlignment.center, 
+              children: List.generate(
+                100,
+                (index) => Icon(
+                  Iconsax.add_circle,
+                  size: index.toDouble(), 
+                  color: const Color.fromARGB(255, 21, 0, 255),
+                ),
               ),
-              Icon(
-               Icons.ac_unit,
-               size: 50, 
-               color: const Color.fromARGB(255, 44, 214, 78),
-              ),
-              Icon(
-               Icons.mail,
-               size: 50, 
-               color: const Color.fromARGB(255, 212, 66, 245),
-              ),
-              Icon(
-               Icons.star,
-               size: 50, 
-               color: const Color.fromARGB(255, 32, 25, 161),
-              ),
-            ],
+            ),
           ),
         ),
       ),
